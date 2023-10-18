e30f7154042b1b516ecdde6e8da9bd72ef7c3ea2
abdullah <abdullah.alhumsi9@gmail.com>
2023-10-18 10:20:16 +0300

    task to sure if user make like is done

--- resources/views/site/facebook/facebookShare.blade.php
+++ resources/views/site/facebook/facebookShare.blade.php
@@ -1,85 +1,102 @@
 @extends('site.layouts.master')
 @section('page_name')
-    {{trans('site.facebook')}} | {{trans('site.makasb')}}
+    {{ trans('site.facebook') }} | {{ trans('site.makasb') }}
 @endsection
 @section('site_css')
-    <link rel="stylesheet" href="{{asset('assets/site/css')}}/bootstrap.min.css"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
-    @if(\Illuminate\Support\Facades\App::getLocale() == 'en')
-        <link rel="stylesheet" href="{{asset('assets/site/css')}}/Conis.css"/>
+    <link rel="stylesheet" href="{{ asset('assets/site/css') }}/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+    @if (\Illuminate\Support\Facades\App::getLocale() == 'en')
+        <link rel="stylesheet" href="{{ asset('assets/site/css') }}/Conis.css" />
     @else
-        <link rel="stylesheet" href="{{asset('assets/site/css')}}/conis_ar.css"/>
+        <link rel="stylesheet" href="{{ asset('assets/site/css') }}/conis_ar.css" />
     @endif
 @endsection
 @section('content')
-    <!--    --><?php //request()->headers->set('X-Frame-Options', 'SAMEORIGIN', false) ?>
+    <!--    --><?php //request()->headers->set('X-Frame-Options', 'SAMEORIGIN', false)
+    ?>
     @include('site.layouts.social-navbar')
     <div class="MainPage d-flex">
-        
+
         @include('site.HomePage.sidebar')
         <div class="Home Share">
             <div class="container sectionHight">
                 <div class="row  align-items-center">
                     <div class="col-lg-6 col-md-6 col-sm-12">
-                        <div><img src="{{asset('assets/site/img')}}/Smiley face-amico.svg" alt=""></div>
+                        <div><img src="{{ asset('assets/site/img') }}/Smiley face-amico.svg" alt=""></div>
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12">
                         <div class="shareBox">
-                            <h3 class="fs-4 fw-bold TitlePage"> {{trans('site.facebook_share')}}  </h3>
-                            <h3 class="fs-5 fw-bold TitlePage2">  {{trans('site.get FREE points by liking')}}</h3>
+                            <h3 class="fs-4 fw-bold TitlePage"> {{ trans('site.facebook_share') }} </h3>
+                            <h3 class="fs-5 fw-bold TitlePage2"> {{ trans('site.get FREE points by liking') }}</h3>
                             <p class="fs-6 mt-lg-3 lh-lg">
 
-                                {{trans('site.To get free points by sharing')}}
+                                {{ trans('site.To get free points by sharing') }}
                             </p>
                             <div>
-                                <button class="mainButton"><a href="{{route('AddSite', $type)}}">
-                                        {{trans('site.Get Facebook Share')}}
+                                <button class="mainButton"><a href="{{ route('AddSite', $type) }}">
+                                        {{ trans('site.Get Facebook Share') }}
                                     </a></button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            @if($data->count() > 0)
+            @if ($data->count() > 0)
                 <div class="container sectionHight2">
-                    @if($data->count())
+                    @if ($data->count())
                         <div class="table">
                             <table class="table-bordered table">
                                 <tr>
-                                    <th>{{trans('site.points')}}</th>
-                                    <th>{{trans('site.name')}}</th>
+                                    <th>{{ trans('site.points') }}</th>
+                                    <th>{{ trans('site.name') }}</th>
                                     <th>{{ trans('site.actions') }}</th>
                                 </tr>
-                                @foreach($data as $row)
-                                    @if($row->points_for_click < $row->user->balance)
-                                        <tr id="row{{$row->id}}">
-                                            <td>{{$row->points_for_click}}</td>
-                                            <td>{{$row->title}}</td>
-                                            <td>
-                                                <div class="text-center">
-                                                    <button data-site-id={{$row->id}} data-url="{{$row->url}}"
-                                                            class="Deletes customBtn StartBtn mb-2">
-                                                        {{trans('site.start')}}
-                                                    </button>
-                                                    <button target="_blank" href="{{$row->share}}"
-                                                            data-site-id={{$row->id}} data-url="{{$row->share}}" data-user-id={{ auth()->user()->id }}
-                                                            class="Deletes d-none customBtn myShareBtn"
-                                                    >{{trans('site.done')}}
-                                                    </button>
-                                                    <button class="Delete customBtn skipBtn"
-                                                            onclick="HideFrame($(this).attr(('data-id')),$(this).attr('data-url'))"
-                                                            data-url="{{$row->url}}"
-                                                            data-id="{{$row->id}}">{{trans('site.skip')}}
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>
+                                @foreach ($data as $row)
+                                    @php
+                                        $confirm = App\Models\ConfirmationTask::where('user_id', auth()->user()->id)
+                                            ->where('site_id', $row->id)
+                                            ->first();
+
+                                    @endphp
+                                    @if (auth()->user()->id != $row->user_id)
+                                        @if ($row->points_for_click < $row->user->balance)
+                                            <tr id="row{{ $row->id }}">
+                                                <td>{{ $row->points_for_click }}</td>
+                                                <td>{{ $row->title }}</td>
+                                                <td>
+                                                    <div class="text-center">
+                                                        @if ($confirm && $confirm->status == 'un_confirmed')
+                                                            <button class="Deletes customBtn">جاري المعالجه سيصلك
+                                                                اشعار</button>
+                                                        @else
+                                                            <button data-site-id={{ $row->id }}
+                                                                data-url="{{ $row->url }}"
+                                                                class="Deletes customBtn StartBtn mb-2">
+                                                                {{ trans('site.start') }}
+                                                            </button>
+                                                            <button target="_blank" href="{{ $row->share }}"
+                                                                data-site-id={{ $row->id }}
+                                                                data-url="{{ $row->share }}"
+                                                                data-user-id={{ auth()->user()->id }}
+                                                                class="Deletes d-none customBtn myShareBtn">{{ trans('site.done') }}
+                                                            </button>
+                                                            <button class="Delete customBtn skipBtn"
+                                                                onclick="HideFrame($(this).attr(('data-id')),$(this).attr('data-url'))"
+                                                                data-url="{{ $row->url }}"
+                                                                data-id="{{ $row->id }}">{{ trans('site.skip') }}
+                                                            </button>
+                                                        @endif
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endif
                                     @endif
                                 @endforeach
                             </table>
                             <!-- Modal -->
 
-                            <div class="modal fade" id="imageUploadModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="imageUploadModal" tabindex="-1" role="dialog"
+                                aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
@@ -92,7 +109,8 @@
                                             ...
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-secondary"
+                                                data-dismiss="modal">Close</button>
                                             <button type="button" class="btn btn-primary">Save changes</button>
                                         </div>
                                     </div>
@@ -100,17 +118,17 @@
                             </div>
                             {!! $data->links() !!}
                         </div>
-                        {{--                        <iframe id="frame" src="">--}}
-                        {{--                        </iframe>--}}
-                        <iframe src="" id="frame" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-                                allowfullscreen="true"
-                                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        {{--                        <iframe id="frame" src=""> --}}
+                        {{--                        </iframe> --}}
+                        <iframe src="" id="frame" style="border:none;overflow:hidden" scrolling="no"
+                            frameborder="0" allowfullscreen="true"
+                            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                     @else
                         @include('site.layouts.empty_data')
                     @endif
                 </div>
             @else
-                <h3 class="fs-4 fw-bold TitlePage">{{trans('site.Wait 9-10 seconds')}}
+                <h3 class="fs-4 fw-bold TitlePage">{{ trans('site.Wait 9-10 seconds') }}
                 </h3>
                 @include('site.layouts.empty_data')
             @endif
@@ -123,9 +141,12 @@
                 }
 
                 iframe {
-                    display: block; /* iframes are inline by default */
-                    border: none; /* Reset default border */
-                    height: 100vh; /* Viewport-relative units */
+                    display: block;
+                    /* iframes are inline by default */
+                    border: none;
+                    /* Reset default border */
+                    height: 100vh;
+                    /* Viewport-relative units */
                     width: 84vw;
                 }
             </style>
@@ -154,11 +175,9 @@
             </div>
         </div>
     </div>
-
 @endsection
 @section('site_js')
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-    <script src="{{asset('assets/site/JS')}}/Conis.js"></script>
+    <script src="{{ asset('assets/site/JS') }}/Conis.js"></script>
     @include('site.CustomScripts.handleViewScript')
 @endsection
-
--- resources/views/site/sites_types/my_site.blade.php
+++ resources/views/site/sites_types/my_site.blade.php
@@ -1,18 +1,19 @@
 @extends('site.layouts.master')
 @section('page_name')
-    {{trans('site.my_site')}} | {{trans('site.makasb')}}
+    {{ trans('site.my_site') }} | {{ trans('site.makasb') }}
 @endsection
 @section('site_css')
-    {{--        <link rel="stylesheet" href="{{asset('assets/site/css')}}/bootstrap.min.css"/>--}}
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
-    @if(\Illuminate\Support\Facades\App::getLocale() == 'en')
-        <link rel="stylesheet" href="{{asset('assets/site/css')}}/Conis.css"/>
+    {{--        <link rel="stylesheet" href="{{asset('assets/site/css')}}/bootstrap.min.css"/> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css" />
+    @if (\Illuminate\Support\Facades\App::getLocale() == 'en')
+        <link rel="stylesheet" href="{{ asset('assets/site/css') }}/Conis.css" />
     @else
-        <link rel="stylesheet" href="{{asset('assets/site/css')}}/conis_ar.css"/>
+        <link rel="stylesheet" href="{{ asset('assets/site/css') }}/conis_ar.css" />
     @endif
 @endsection
 @section('content')
-    <!--    --><?php //request()->headers->set('X-Frame-Options', 'SAMEORIGIN', false) ?>
+    <!--    --><?php //request()->headers->set('X-Frame-Options', 'SAMEORIGIN', false)
+    ?>
     @include('site.layouts.social-navbar')
     <div class="MainPage d-flex">
         @include('site.HomePage.sidebar')
@@ -20,18 +21,18 @@
             <div class="container sectionHight">
                 <div class="row  align-items-center">
                     <div class="col-lg-6 col-md-6 col-sm-12">
-                        <div><img src="{{asset('assets/site/img')}}/Smiley face-amico.svg" alt=""></div>
+                        <div><img src="{{ asset('assets/site/img') }}/Smiley face-amico.svg" alt=""></div>
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-12">
                         <div class="shareBox">
-                            <h3 class="fs-4 fw-bold TitlePage"> {{ trans_model($siteType, 'main_word') }}  </h3>
-                            <h3 class="fs-5 fw-bold TitlePage2">  {{ trans_model($siteType, 'submain_word') }}</h3>
+                            <h3 class="fs-4 fw-bold TitlePage"> {{ trans_model($siteType, 'main_word') }} </h3>
+                            <h3 class="fs-5 fw-bold TitlePage2"> {{ trans_model($siteType, 'submain_word') }}</h3>
                             <p class="fs-6 mt-lg-3 lh-lg">
 
                                 {{ trans_model($siteType, 'description') }}
                             </p>
                             <div>
-                                <button class="mainButton"><a href="{{route('AddSite', $type)}}">
+                                <button class="mainButton"><a href="{{ route('AddSite', $type) }}">
                                         {{ trans_model($siteType, 'button_word') }}
                                     </a></button>
                             </div>
@@ -41,50 +42,66 @@
                 </div>
             </div>
             <div class="container sectionHight2">
-                @if($data->count())
+                @if ($data->count())
                     <div class="table">
                         <table class="table-bordered table">
                             <tr>
-                                <th>{{trans('site.points')}}</th>
-                                <th>{{trans('site.name')}}</th>
+                                <th>{{ trans('site.points') }}</th>
+                                <th>{{ trans('site.name') }}</th>
                                 <th>{{ trans('site.actions') }}</th>
                             </tr>
-                            @foreach($data as $row)
-                                {{--                                {{dd($row)}}--}}
-                                @if($row->points_for_click < $row->user->balance)
-                                    <tr id="row{{$row->id}}">
-                                        <td>{{$row->points_for_click}}</td>
-                                        <td>{{$row->title}}</td>
-                                        <td>
-                                            <div class="text-center">
-                                                <input type="hidden" id="time_out" name="" value="{{ $siteType->time_out_seconds }}"/>
-                                                <button data-site-id={{$row->id}} data-url="{{$row->url}}"
-                                                        class="Deletes customBtn StartBtn">
-                                                    {{trans('site.start')}}
-                                                </button>
-                                                <button target="_blank" href="{{$row->share}}" data-site-id={{$row->id}} data-url="{{$row->share}}" data-user-id={{ auth()->user()->id }}
-                                                        class="Deletes d-none customBtn myShareBtn"
-                                                >{{trans('site.done')}}
-                                                </button>
-                                                <button class="Delete customBtn skipBtn"
-                                                        onclick="HideFrame($(this).attr(('data-id')),$(this).attr('data-url'))"
-                                                        data-url="{{$row->url}}"
-                                                        data-id="{{$row->id}}">{{trans('site.skip')}}
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
+                            @foreach ($data as $row)
+                                @php
+                                    $confirm = App\Models\ConfirmationTask::where('user_id', auth()->user()->id)
+                                        ->where('site_id', $row->id)
+                                        ->first();
+
+                                @endphp
+                                @if (auth()->user()->id != $row->user_id)
+                                    @if ($row->points_for_click < $row->user->balance)
+                                        <tr id="row{{ $row->id }}">
+                                            <td>{{ $row->points_for_click }}</td>
+                                            <td>{{ $row->title }}</td>
+                                            <td>
+                                                <div class="text-center">
+                                                    @if ($confirm && $confirm->status == 'un_confirmed')
+                                                        <button class="Deletes customBtn">جاري المعالجه سيصلك
+                                                            اشعار</button>
+                                                    @else
+                                                        <input type="hidden" id="time_out" name=""
+                                                            value="{{ $siteType->time_out_seconds }}" />
+                                                        <button data-site-id={{ $row->id }}
+                                                            data-url="{{ $row->url }}"
+                                                            class="Deletes customBtn StartBtn">
+                                                            {{ trans('site.start') }}
+                                                        </button>
+                                                        <button target="_blank" href="{{ $row->share }}"
+                                                            data-site-id={{ $row->id }}
+                                                            data-url="{{ $row->share }}"
+                                                            data-user-id={{ auth()->user()->id }}
+                                                            class="Deletes d-none customBtn myShareBtn">{{ trans('site.done') }}
+                                                        </button>
+                                                        <button class="Delete customBtn skipBtn"
+                                                            onclick="HideFrame($(this).attr(('data-id')),$(this).attr('data-url'))"
+                                                            data-url="{{ $row->url }}"
+                                                            data-id="{{ $row->id }}">{{ trans('site.skip') }}
+                                                        </button>
+                                                    @endif
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endif
                                 @endif
                             @endforeach
                         </table>
 
                         {!! $data->links() !!}
                     </div>
-                    {{--                        <iframe id="frame" src="">--}}
-                    {{--                        </iframe>--}}
+                    {{--                        <iframe id="frame" src=""> --}}
+                    {{--                        </iframe> --}}
                     <iframe src="" id="frame" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
-                            allowfullscreen="true"
-                            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+                        allowfullscreen="true"
+                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
                 @else
                     @include('site.layouts.empty_data')
                 @endif
@@ -98,33 +115,37 @@
                 }
 
                 iframe {
-                    display: block; /* iframes are inline by default */
-                    border: none; /* Reset default border */
-                    height: 100vh; /* Viewport-relative units */
+                    display: block;
+                    /* iframes are inline by default */
+                    border: none;
+                    /* Reset default border */
+                    height: 100vh;
+                    /* Viewport-relative units */
                     width: 84vw;
                 }
             </style>
         </div>
     </div>
-
 @endsection
 @section('site_js')
     <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-    <script src="{{asset('assets/site/JS')}}/Conis.js"></script>
+    <script src="{{ asset('assets/site/JS') }}/Conis.js"></script>
     <script>
-        $('.StartBtn').on('click', function (e) {
+        $('.StartBtn').on('click', function(e) {
             var time = $('#time_out').val();
             var elem = $(this);
             var url = $(this).attr('data-url');
 
             window.open(url, "_blank");
 
-            setTimeout(function () {
-                toastr.error('تخطيت الوقت المسموح .. برجاء المحاولة مره اخري', {timeout: 1500});
-                setTimeout(function () {
+            setTimeout(function() {
+                toastr.error('تخطيت الوقت المسموح .. برجاء المحاولة مره اخري', {
+                    timeout: 1500
+                });
+                setTimeout(function() {
                     window.location.reload();
-                },2000)
-            }, {{($siteType->time_out_seconds + 10) * 1000}})
+                }, 2000)
+            }, {{ ($siteType->time_out_seconds + 10) * 1000 }})
 
             var timerId = setInterval(countdown, 1000);
 
@@ -165,6 +186,4 @@
             </div>
         </div>
     </div>
-
 @endsection
-
